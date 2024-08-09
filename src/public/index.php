cf953c85b0d6fedd0c3c962bd1027c4d4374feff
Guilherme Rangel <groliveiranet@hotmail.com>
2024-08-09 16:52:50 -0300

    Remover controller nao utilizado no index

--- src/public/index.php
+++ src/public/index.php
@@ -2,7 +2,6 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
-use App\Controllers\HomeController;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
